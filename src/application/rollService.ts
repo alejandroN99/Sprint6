9f78a1537eb60df64f1dd1ec88e63c2fe0347a7f
Juan Estrella <juan.estrella@finos.org>
2023-06-26 11:18:12 +0200

    add getAllPlayers and playerRoll functions

--- src/application/rollService.ts
+++ src/application/rollService.ts
@@ -1,16 +1,25 @@
+import { Player } from '../domain/player';
+
 export const roll = () => {
-    return Math.floor(Math.random() * 6) + 1;
+	return Math.floor(Math.random() * 6) + 1;
 };
 
 export const total = (roll1: number, roll2: number): number => {
-    return roll1 + roll2;
+	return roll1 + roll2;
 };
 
-export const playGame = (): string => {
-    const roll1 = roll();
-    const roll2 = roll();
-    const totalRoll = total(roll1, roll2);
-    
-    const result = (totalRoll === 7)?'You win!':'You lose!';
-    return result;
+export const playGame = (): object => {
+	const roll1 = roll();
+	const roll2 = roll();
+	const totalRoll = total(roll1, roll2);
+	const result = totalRoll === 7 ? 'You win!' : 'You lose!';
+
+	const rollResult = {
+		roll1,
+		roll2,
+		totalRoll,
+		result
+	};
+
+	return rollResult;
 };
