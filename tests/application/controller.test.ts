f3d7aee3a861f59c4ed77442cbeb874f04166c6b
Axel Piñeros <axelpineros@outlook.com>
2023-06-27 10:48:35 +0200

    Actualitzat tests i describe global

--- tests/application/controller.test.ts
+++ tests/application/controller.test.ts
@@ -1,5 +1,5 @@
 import {
-	createdPlayer,
+	createPlayer,
 	updatePlayerName,
 	players,
 	getAllPlayers,
@@ -9,50 +9,55 @@ import {
 	getWinPercentage
 } from '../../src/application/controller';
 
-describe('Function createdPlayer', () => {
-	test('', () => {
-		const result = createdPlayer('Juan');
-		const expectedResult = 'Player created successfully!';
+describe('Controller tests', () => {
 
-		expect(result).toBe(expectedResult);
+	describe('Function createPlayer', () => {
+		test('', () => {
+			const playerName = 'Juan'
+			const result = createPlayer(playerName);
+			const expectedResult = `Player ${playerName} created successfully!`;
+
+			expect(result).toBe(expectedResult);
+		});
 	});
-});
 
-describe('Function updatePlayerName', () => {
-	test('', () => {
-		const result = updatePlayerName('Juan', 'Pepe');
-		const expectedResult = 'Update player name successfully!';
+	describe('Function updatePlayerName', () => {
+		test('', () => {
+			const playerNewName = 'Pepe'
+			const result = updatePlayerName(0, playerNewName);
+			const expectedResult = `Player is now called ${playerNewName}!`;
 
-		expect(result).toBe(expectedResult);
+			expect(result).toBe(expectedResult);
+		});
 	});
-});
 
-describe('Function getAllPlayers', () => {
-	test('should get all players', () => {
-		expect(getAllPlayers()).toEqual(players);
+	describe('Function getAllPlayers', () => {
+		test('should get all players', () => {
+			expect(getAllPlayers()).toEqual(players);
+		});
 	});
-});
 
-describe('Function playerRoll', () => {
-	test('should play the game', () => {
-		expect(playerRoll(0)).toEqual(players[0].rolls[0]);
+	describe('Function playerRoll', () => {
+		test('should play the game', () => {
+			expect(playerRoll(0)).toEqual(players[0].rolls[0]);
+		});
 	});
-});
 
-describe('Function getAllPlayerRolls', () => {
-	test('should get all rolls from a player', () => {
-		expect(getAllPlayerRolls(0)).toEqual(players[0].rolls);
+	describe('Function getAllPlayerRolls', () => {
+		test('should get all rolls from a player', () => {
+			expect(getAllPlayerRolls(0)).toEqual(players[0].rolls);
+		});
 	});
-});
 
-describe('Get winPercentage', () => {
-	test('should return the win percentage of a player', () => {
-		expect(getWinPercentage(0)).toBe(players[0].winPercentage);
+	describe('Get winPercentage', () => {
+		test('should return the win percentage of a player', () => {
+			expect(getWinPercentage(0)).toBe(players[0].winPercentage);
+		});
 	});
-});
 
-describe('Function deletePlayerRolls', () => {
-	test('should delete all rolls from a player', () => {
-		expect(deletePlayerRolls(0)).toEqual((players[0].rolls = []));
+	describe('Function deletePlayerRolls', () => {
+		test('should delete all rolls from a player', () => {
+			expect(deletePlayerRolls(0)).toEqual((players[0].rolls = []));
+		});
 	});
 });
