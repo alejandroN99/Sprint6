8c681f0bfdb6002cccc00c7d8641eeffdada1fc5
Axel Piñeros <axelpineros@outlook.com>
2023-06-27 10:47:55 +0200

    Object a any

--- src/infrastructure/utilities/find.ts
+++ src/infrastructure/utilities/find.ts
@@ -2,5 +2,5 @@ import { Player } from '../../domain/player';
 
 // find from array
 export const find = (array: Player[], key: string, value: number) => {
-	return array.find((item: object) => item[key] === value);
+	return array.find((item: any) => item[key] === value);
 };
