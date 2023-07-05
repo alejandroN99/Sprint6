a1e93d5a049a3745c7bf9329bd050063e9438612
Axel Piñeros <axelpineros@outlook.com>
2023-07-05 12:12:13 +0200

    Primer intent arxiu mongoose

--- src/infrastructure/mongoose.ts
+++ src/infrastructure/mongoose.ts
@@ -1,16 +1,27 @@
-const mongoose = require('mongoose');
+import mongoose, { Schema, Document } from 'mongoose';
 
 mongoose.connect('mongodb://localhost:27017/prueba', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 })
     .then(() => {
         console.log('Conexión exitosa a la base de datos');
     })
-    .catch((error: any) => {
+    .catch((error: Error) => {
         console.error('Error al conectar a la base de datos:', error);
     });
 
+// Definir el tipo para el modelo de jugador
+interface IPlayer extends Document {
+    id: number;
+    name: string;
+    date: Date;
+    winPercentage: number;
+}
+
+// Definir el esquema para la secuencia de ID
+interface ISequence extends Document {
+    name: string;
+    value: number;
+}
 
 // Evento de conexión exitosa
 mongoose.connection.on('connected', () => {
@@ -18,46 +29,112 @@ mongoose.connection.on('connected', () => {
 });
 
 // Evento de error en la conexión
-mongoose.connection.on('error', (error: any) => {
+mongoose.connection.on('error', (error: Error) => {
     console.error('Error en la conexión a MongoDB:', error);
 });
 
-
-
-// Define el esquema
-const Schema = mongoose.Schema;
-
-const playerSchema = new Schema({
-    id: Number,
+// Define el esquema para la secuencia de ID
+const sequenceSchema: Schema<ISequence> = new Schema({
     name: String,
-    date: String,
-    winPercentage: Number
+    value: {
+        type: Number,
+        default: 1,
+    },
 });
 
-// Crea un modelo a partir del esquema
-const Player = mongoose.model('Player', playerSchema);
+// Crea un modelo para la secuencia de ID
+const Sequence = mongoose.model<ISequence>('Sequence', sequenceSchema);
 
-// Crea un nuevo jugador
-const nuevoJugador = new Player({
-    id: 1,
-    name: 'John Doe',
-    date: '2023-07-04',
-    winPercentage: 75.5
+// Define el esquema para el jugador
+const playerSchema: Schema<IPlayer> = new Schema({
+    id: {
+        type: Number,
+        unique: true,
+    },
+    name: String,
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    winPercentage: Number,
 });
 
-// Guarda el jugador en la base de datos
-nuevoJugador.save()
-    .then(() => {
+// Crea un modelo para el jugador
+const Player = mongoose.model<IPlayer>('Player', playerSchema);
+
+(async () => {
+    try {
+        // Obtiene la secuencia actualizada o crea una nueva si no existe
+        let sequence: ISequence | null = await Sequence.findOne({ name: 'playerId' });
+        if (!sequence) {
+            sequence = new Sequence({ name: 'playerId' });
+        }
+
+        // Asigna el ID al nuevo jugador
+        const nuevoJugador: IPlayer = new Player({
+            id: sequence.value,
+            name: 'John Doe',
+            winPercentage: 0,
+        });
+
+        // Guarda el jugador en la base de datos
+        await nuevoJugador.save();
+
+        // Incrementa el valor de la secuencia
+        sequence.value += 1;
+        await sequence.save();
+
         console.log('Jugador creado exitosamente');
-    })
-    .catch((error: any) => {
+    } catch (error) {
         console.error('Error al crear el jugador:', error);
-    });
+    }
+})();
 
 Player.find()
-    .then((jugadores:any) => {
+    .then((jugadores: IPlayer[]) => {
         console.log('Jugadores encontrados:', jugadores);
     })
-    .catch((error:any) => {
+    .catch((error: Error) => {
         console.error('Error al leer los jugadores:', error);
     });
+
+async function deletePlayerById(playerId: number): Promise<void> {
+    try {
+        // Busca y elimina al jugador por su ID
+        const result = await Player.deleteOne({ id: playerId });
+
+        if (result.deletedCount && result.deletedCount > 0) {
+            console.log('Jugador eliminado exitosamente');
+        } else {
+            console.log(`No se encontró ningún jugador con el ID ${playerId}`);
+        }
+    } catch (error) {
+        console.error('Error al eliminar el jugador:', error);
+    }
+}
+
+deletePlayerById(1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Eliminar un jugador por su ID
+// Player.deleteOne({ _id: jugadorId })
+//     .then(() => {
+//         console.log('Jugador eliminado exitosamente');
+//     })
+//     .catch((error:any) => {
+//         console.error('Error al eliminar el jugador:', error);
+//     });
